5abf2813e17954825b73eedbdea974f7fa155e3a
Aayush Singh <202117b3731@wilp.bits-pilani.ac.in>
2024-03-22 13:53:59 +0530

    new css added

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: ['400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700','800'],
   variable: '--font-ibm-plex'
 });
 
